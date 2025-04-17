9b6aec64479f7ab1fd832d618f57decb835806a0
Sachith Lakmal <sachithlakmal@icloud.om>
2025-04-17 13:55:14 +0530

    functions with return type, multiple return types, best practices

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -5,11 +5,11 @@ import (
 	"math"
 )
 
+const inflationRate = 6.5
+
 func main() {
 
 	fmt.Println("Calculating the investment")
-
-	const inflationRate = 6.5
 
 	var investmentAmount float64 = 1000
 	var expectedRateOfReturn = 5.5
@@ -26,13 +26,19 @@ func main() {
 	fmt.Print("Please enter the no of years: ")
 	_, _ = fmt.Scan(&years)
 
-	calculateInvestment(investmentAmount, expectedRateOfReturn, years, inflationRate)
+	futureValue, futureRealValue := calculateInvestment(investmentAmount, expectedRateOfReturn, years, inflationRate)
+	output(futureValue, futureRealValue)
 }
 
-func calculateInvestment(investmentAmount, expectedRateOfReturn, years, inflationRate float64) {
-	futureValue := investmentAmount * math.Pow(1+expectedRateOfReturn/100, years)
-	futureRealValue := futureValue * math.Pow(1+inflationRate/100, years)
+func calculateInvestment(investmentAmount, expectedRateOfReturn, years, inflationRate float64) (futureValue float64, futureRealValue float64) {
 
+	futureValue = investmentAmount * math.Pow(1+expectedRateOfReturn/100, years)
+	futureRealValue = futureValue * math.Pow(1+inflationRate/100, years)
+
+	return futureValue, futureRealValue
+}
+
+func output(futureValue, futureRealValue float64) {
 	fmt.Printf("Future value is: %v \n", futureValue)
 	fmt.Printf("Future real value is: %v \n", futureRealValue)
 }
