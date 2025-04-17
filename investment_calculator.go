f0db833e2ef8a9acb059a9cb0ccef5e1aae737d1
Sachith Lakmal <sachithlakmal@icloud.om>
2025-04-17 08:39:35 +0530

    one liner ;)

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,9 +8,7 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 
-	investmentAmount := 1000.0
-	expectedRateOfReturn := 5.5
-	years := 10.0
+	investmentAmount, expectedRateOfReturn, years := 1000.0, 5.5, 10.0
 
 	futureValue := investmentAmount * math.Pow(1+expectedRateOfReturn/100, years)
 
