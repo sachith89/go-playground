c8c6365044dbdff809f3245e67348244c7701b44
Sachith Lakmal <sachithlakmal@icloud.om>
2025-04-17 08:41:31 +0530

    this approach is more readable and easy to troubleshoot when the things are not perfect

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,9 +8,11 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 
-	var investmentAmount, expectedRateOfReturn, years float64 = 1000, 5.5, 10
+	var investmentAmount float64 = 1000
+	var expectedRateOfReturn float64 = 5.5
+	var years float64 = 10
 
-	futureValue := investmentAmount * math.Pow(1+expectedRateOfReturn/100, years)
+	var futureValue = investmentAmount * math.Pow(1+expectedRateOfReturn/100, years)
 
 	fmt.Println(futureValue)
 }
