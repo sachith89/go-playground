601b245f621e53c96a9df6cec510f8bf9f3a81db
Sachith Lakmal <sachithlakmal@icloud.om>
2025-04-17 08:32:20 +0530

    float64

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var expectedRateOfReturn = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedRateOfReturn/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedRateOfReturn/100, years)
 
 	fmt.Println(futureValue)
 }
