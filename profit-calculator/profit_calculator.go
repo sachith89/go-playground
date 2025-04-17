189f4c4f3516a84204f64fb2342653ecc951a001
Sachith Lakmal <sachithlakmal@icloud.om>
2025-04-17 13:16:29 +0530

    Sprintf

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -21,7 +21,9 @@ func main() {
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 
+	formattedTaxRate := fmt.Sprintf("Earnings Before Tax to Profit After Tax Ratio: %.2f\n", ratio)
+
 	fmt.Printf("Earnings Before Tax (EBT): %.2f\n", ebt)
 	fmt.Printf("Profit After Tax: %.2f\n", profit)
-	fmt.Printf("Earnings Before Tax to Profit After Tax Ratio: %.2f\n", ratio)
+	fmt.Printf(formattedTaxRate)
 }
